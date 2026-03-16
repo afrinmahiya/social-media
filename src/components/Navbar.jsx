6a53177433a9f8971f034d80cc04bb4406569f09
afrinmahiya <afrinmahiya55@gmail.com>
2026-03-16 10:02:17 +0600

    navbar & sidebar desing done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { FaBars } from "react-icons/fa6";
+import { BsThreeDots } from "react-icons/bs";
+import { IoSearch } from "react-icons/io5";
+import { FaCartArrowDown } from "react-icons/fa";
+import { BiLogoMessenger } from "react-icons/bi";
+import { FaBell } from "react-icons/fa";
+import { IoSettings } from "react-icons/io5";
+
+
+
+
+const Navbar = () => {
+  return (
+    <nav className='bg-[#615DFA] flex items-center max-[1628px] font-Poppins '>
+      <div className='pl-6 flex gap-3 ' >
+        <img src="/images/logo.png" alt="Logo" />
+
+      </div>
+       
+        <ul className='flex gap-12 items-center  text-white  ml-[73px] mr-[73px]'>
+          <li ><FaBars /></li>
+          <li className='text-[14px]'>Careers</li>
+          <li className=' text-[14px]'>Store</li>
+          <li className='text-[14px] '>FAQ</li>
+          <li  className='text-[20px] '><BsThreeDots /></li>
+        </ul>
+
+        <div className='max-w-103.75 rounded-[11px] bg-[#4E4AC8] relative py-4.5 px-4 mr-[70px]'>
+          <input className="text-[#6965E0]" placeholder='Search here ....' type='text'/>
+          <div className='absolute text-[#6763DF] top-[30%] right-[26px] w-3.5  h-3.5'>
+
+           <IoSearch /> </div>
+         </div>
+
+
+     <div className='mr-[40px]'>
+      <div className=' max-w-27.75 flex gap-11.5 font-Poppins font-normal text-xs text-white mb-0.75 '>
+        <p> Next</p>
+        <p>35 EPX</p>
+      </div>
+      <div className=' bg-[#4E4AC8] h-0.75 max-w-27.75 '><div className='max-w-[83px] bg-[#41EFFF] h-0.75 '>
+     </div></div>
+     </div>
+
+     <div>
+      <ul className='flex '>
+        <li className=' w-8 h-px rotate-90 bg-[#7A77FD] mt-2.75'></li>
+
+         <li className='w-4.5 h-4.5 text-[#7A77FD]'><FaCartArrowDown />
+</li>
+          <li className='w-4.5 h-4.5 text-[#7A77FD]'><BiLogoMessenger />
+</li>
+           <li className='w-4.5 h-4.5 text-[#7A77FD]'><FaBell />
+</li>
+            <li className='w-8 h-px  bg-[#7A77FD] rotate-90 mt-2.75'></li>
+             <li className='w-4.5 h-4.5 text-[#7A77FD]'><IoSettings />
+
+</li>
+
+      </ul>
+     </div>
+    
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,64 @@
+import { FaNewspaper } from "react-icons/fa";
+import { FaFileVideo } from "react-icons/fa";
+import { FaLayerGroup } from "react-icons/fa";
+import { MdOutlineMonochromePhotos } from "react-icons/md";
+import { FaUserFriends } from "react-icons/fa";
+import { FaHandsAslInterpreting } from "react-icons/fa6";
+import { MdLogout } from "react-icons/md";
+
+
+
+const Sidebar = () => {
+  return (
+    <div className="max-w-[300px]">
+        <div className=" relative "> <img src="/public/images/cover-pic.png" alt="" /></div>
+           <div className=" absolute top-20 left-22.5 border-4 rounded-[50%] border-white "> <img src="/public/images/porfile-pic.png" alt="" /></div>
+     
+     <div className="username font-Poppins mt-[75px] px-[68px] text-center ">
+        <h1 className="font-bold text-[16px] text-black  "> MAHIYA AFRIN</h1>
+        <p className="font-medium  text-[12px] text-[#AFB0C0] ">www.maahiya.com</p>
+     </div>
+
+     <div className="mt-[42px] flex  ">
+         <div className="pl-[35px] pr-[24px] "> 
+        <h4 className=" font-bold text-[14px] text-black  ">930</h4>
+        <p className="font-bold text-[12px] text-[#AFB0C0]">Friends</p>
+         </div>
+
+          <div className=' w-[1px] h-[25px] font-bold bg-[#AFB0C0] mt-2.75'></div>
+
+          <div className="pl-[24px] pr-[24px]">
+        <h4 className=" font-bold text-[14px] text-black  ">87</h4>
+        <p className="font-bold text-[12px] text-[#AFB0C0]">Posts</p>
+        </div>
+
+          <div className=' w-[1px] h-[25px] font-bold bg-[#AFB0C0] mt-2.75'></div>
+
+           <div className="pl-[24px] pr-[34px]">
+        <h4 className=" font-bold text-[14px] text-black  ">1k</h4>
+          <p className="font-bold text-[12px] text-[#AFB0C0]">Followers</p>
+           </div>
+     </div>
+
+     
+     <div className="mt-[64px] text-[#AFB0C0] font-semibold text-[14px] mr-[8px] ml-[24px]  ">
+
+      <ul>
+        <li className="w-[268px] h-[50px] flex gap-[29px] "> <div className="pt-[5px]"> <FaNewspaper /></div> Newsfeed </li>
+        <li className="w-[268px] h-[50px] flex gap-[29px]"><div className="pt-[5px]"> <FaFileVideo /> </div>Videos </li>
+        <li className="w-[268px] h-[50px] flex gap-[29px]"><div className="pt-[5px]"> <FaLayerGroup /></div> Groups </li>
+        <li className="w-[268px] h-[50px] flex gap-[29px]"><div className="pt-[5px]"> <MdOutlineMonochromePhotos /></div> Photos </li>
+        <li className=" w-[268px] h-[50px] flex gap-[29px]"> <div className="pt-[5px]"><FaUserFriends /></div>  Friends </li>
+        <li className="w-[268px] h-[50px] flex gap-[29px]"><div className="pt-[5px]"><FaHandsAslInterpreting /></div>   Friends Request </li>
+        <li className=" flex gap-[29px]"><div className="pt-[5px]"><MdLogout /></div>  Logout </li>
+      </ul>
+
+
+     </div>
+
+
+    </div>
+  )
+}
+
+export default Sidebar
